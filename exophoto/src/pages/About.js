ce27172a916f6aec3ae6523df59c9dd07df587e7
Billy Pinkerton <billybattlebones@gmail.com>
2023-03-07 15:24:59 -0700

    fixing typos

--- exophoto/src/pages/About.js
+++ exophoto/src/pages/About.js
@@ -2,7 +2,7 @@
 const About = () => (
 <div className='flex flex-auto justify-center '>
     <div className='container flex flex-wrap justify-center  rounded-md mt-auto max-w-md border-2 py-1 px-2 bg-slate-600 text-zinc-50 border-gray-900'>
-        EXOPhoto is a project that has begun, in a way, to learn and grow my skills in web design. EXOPhoto is under construction. If your interested in photography, art, design and how we can incorporate those interests in the preservation of the environment, please follow along and perhaps join up to help keep keep our homes and nature beautiful.
+        EXOPhoto is a project that has begun, in a way, to learn and grow my skills in web design. EXOPhoto is under construction. If your interested in photography, art, design and how we can incorporate those interests in the preservation of the environment, please follow along and perhaps join up to help keep our homes and nature beautiful.
     </div>
 </div>
 );
